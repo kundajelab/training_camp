2979f717f38dae33f2abd4d10e783411d4e3ad20
oursu <oanamursu@gmail.com>
2015-09-05 16:40:10 -0700

    Working batch peak calling

--- src/batch_call_peaks.sh
+++ src/batch_call_peaks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in $(find ${DATA_DIR}/tagAlign/mergedReplicates -name '*.tagAlign.gz')
 do
-    qsub -V -cwd -l h_vmem=4G -N "S$(basename $i)" -o "${DATA_DIR}/tagAlign/mergedReplicates/${i}.call_peaks.out" -e "${DATA_DIR}/tagAlign/mergedReplicates/${i}.call_peaks.err" ${SRC_DIR}/call_peaks.sh yeast $i
+    qsub -V -cwd -l h_vmem=4G -N "S$(basename $i)" -o "${i}.call_peaks.out" -e "${i}.call_peaks.err" ${SRC_DIR}/call_peaks.sh yeast $i
 done
