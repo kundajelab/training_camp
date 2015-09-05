49c4d57797163eed9658f2bfe20048f19072317e
oursu <oanamursu@gmail.com>
2015-09-05 16:33:37 -0700

    Changes to merge replicates and batch bonvert to bed

--- src/batch_call_peaks.sh
+++ src/batch_call_peaks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in $(find ${DATA_DIR}/tagAlign -name '*.tagAlign.gz')
+for i in $(find ${DATA_DIR}/tagAlign/mergedReplicates -name '*.tagAlign.gz')
 do
-    qsub -V -cwd -q training-camp -A training-camp -l h_vmem=4G -N $(basename $i) -o $i.out -e $i.err ./call_peaks.sh yeast $i
+    qsub -V -cwd -l h_vmem=4G -N "S$(basename $i)" -o "${DATA_DIR}/tagAlign/mergedReplicates/${i}.call_peaks.out" -e "${DATA_DIR}/tagAlign/mergedReplicates/${i}.call_peaks.err" ${SRC_DIR}/call_peaks.sh yeast $i
 done
