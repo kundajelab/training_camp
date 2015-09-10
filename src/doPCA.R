2a64542d816cdd9f4b2771582f3907edc859ddae
AvantiShri <avanti.shrikumar@gmail.com>
2015-09-10 13:22:04 -0700

    PCA rotation files output

--- src/doPCA.R
+++ src/doPCA.R
@@ -9,16 +9,17 @@ row_sub = apply(inpData, 1, function(row) any(row != row[1] ))
 components <- prcomp(t(inpData[row_sub,]),scale=TRUE)
 
 #visualise the sdevs associated with the PCs
-png("PCA_sdev.png")
+#png("PCA_sdev.png")
 barplot(components$sdev,xlab="PCs")
 dev.off()
 
 numComponentsToFocusOn = 3
-for (i in seq(1,numComponentsToFocusOn-1)) {
+for (i in seq(1,numComponentsToFocusOn)) {
     component_i = components$x[,i]
+    write.table(components$rotation[,i],paste("pc",i,"_rotation.txt",sep=""),quote=FALSE,col.names=FALSE,row.names=TRUE)
     for (j in seq(i+1,numComponentsToFocusOn)) {
         component_j = components$x[,j]
-        png(paste(paste("PC",i,"vs",j,sep="_"),".png", sep=""))
+#        png(paste(paste("PC",i,"vs",j,sep="_"),".png", sep=""))
         plot(component_i,component_j,xlab=paste("Component",i,sep=""), ylab=paste("Component",j,sep=""),col="blue")
         text(component_i,component_j,names(inpData),cex=1.0)
         dev.off()
