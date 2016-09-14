d0346339b5bc68acc726aa8fbd920ec3b761cf78
annashcherbina <annashcherbina@gmail.com>
2016-09-14 22:52:14 +0000

    fixed find motifs

--- src/find_motifs.sh
+++ src/find_motifs.sh
@@ -1,16 +1,18 @@
-
+echo $GENOME_FILE_DIR
+echo $MOTIF_DIR
 b=$1 #bed file in which we will search for motifs
-genomeFasta=${YEAST_DIR}/WholeGenomeFasta/genome.fa
-b=/tc2015/oursu/results/peaks/PooledReps_Sample3_arp-ko.4col.peaks.bed
+#genomeFasta=${YEAST_DIR}/WholeGenomeFasta/genome.fa
+genomeFasta=/srv/scratch/training_camp/saccer3/SacCer3_genome.fa
+#b=/tc2015/oursu/results/peaks/PooledReps_Sample3_arp-ko.4col.peaks.bed
 
 
 #get the fasta file corresponding to the bed file
-bedtools getfasta -fi ${genomeFasta} -bed ${b} -fo ${DATA_DIR}/results/motifs/$(basename $b | sed 's/.bed//g').fa
+bedtools getfasta -fi ${genomeFasta} -bed ${b} -fo ${MOTIF_DIR}$(basename $b | sed 's/.bed//g').fa
 
 #make a soft link to the genome file
-genomeLink=${DATA_DIR}/results/motifs/genomeFiles/$(basename ${genomeFasta})
+genomeLink=${GENOME_FILE_DIR}/$(basename ${genomeFasta})
 ln -s ${genomeFasta} ${genomeLink}
 
 #scan for the motif collection in homer 
-findMotifsGenome.pl ${b} ${genomeLink} ${DATA_DIR}/results/motifs/ -size given -len 8
+findMotifsGenome.pl ${b} ${genomeLink} ${MOTIF_DIR} -size given -len 8
 #### remember to set TMP for homer
