12741642a81c442503fd94eb5030d8a8ccfd64b9
annashcherbina <annashcherbina@gmail.com>
2016-09-15 19:59:30 +0000

    motif directory bug fix

--- src/find_motifs.sh
+++ src/find_motifs.sh
@@ -7,12 +7,12 @@ genomeFasta=/srv/scratch/training_camp/saccer3/SacCer3_genome.fa
 
 
 #get the fasta file corresponding to the bed file
-bedtools getfasta -fi ${genomeFasta} -bed ${b} -fo ${MOTIF_DIR}$(basename $b | sed 's/.bed//g').fa
+bedtools getfasta -fi ${genomeFasta} -bed ${b} -fo ${MOTIF_DIR}/$(basename $b | sed 's/.bed//g').fa
 
 #make a soft link to the genome file
-genomeLink=${GENOME_FILE_DIR}/$(basename ${genomeFasta})
-ln -s ${genomeFasta} ${genomeLink}
+#genomeLink=${GENOME_FILE_DIR}/$(basename ${genomeFasta})
+#ln -s ${genomeFasta} ${genomeLink}
 
 #scan for the motif collection in homer 
-findMotifsGenome.pl ${b} ${genomeLink} ${MOTIF_DIR} -size given -len 8
+findMotifsGenome.pl ${b} $genomeFasta ${MOTIF_DIR}/$(basename $b | sed 's/.bed//g') -size given -len 8
 #### remember to set TMP for homer
