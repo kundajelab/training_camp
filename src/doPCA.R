47bd3e9e39ff3ac21602dcbc7639dd348d026f96
AvantiShri <avanti@stanford.edu>
2017-09-22 07:52:29 +0000

    pca

--- src/doPCA.R
+++ src/doPCA.R
@@ -1,9 +1,11 @@
 #!/usr/bin/env Rscript
-library(ggplot2)
 library('Cairo')
 args <- commandArgs(trailingOnly = TRUE)
 inpFile <- args[1] #file with cols to run PCA on; cols are the samples
+batchesFile <- args[2]
+var <-args[3]
 data <- read.table(inpFile, header=TRUE, sep='\t')
+batches=read.table(batchesFile,header=TRUE,sep='\t')
 data.pca=prcomp(t(data),center=FALSE,scale=TRUE)
 CairoPNG("PCA_scree_plot.png")
 barplot(100*data.pca$sdev^2/sum(data.pca$sdev^2),las=2,xlab="",ylab="% Variance Explained")
@@ -20,8 +22,8 @@ for (i in seq(1,numComponentsToFocusOn)) {
     for (j in seq(i+1,numComponentsToFocusOn)) {
         component_j = data.pca$x[,j]
 	CairoPNG(paste(paste("PC",i,"vs",j,sep="_"),".png", sep=""))
-        plot(component_i,component_j,xlab=paste("Component",i,sep=""), ylab=paste("Component",j,sep=""),col="blue")
-        text(component_i,component_j,names(data),cex=0.7)
+        plot(component_i,component_j,xlab=paste("Component",i,sep=""), ylab=paste("Component",j,sep=""),col=batches[,var])
+        text(component_i,component_j,names(data),pos=3,cex=0.5)
         dev.off()
     }
 }
